1998314c1ece6777240e385993a4cbe98bd2ce97
root <anonhq789@protonmail.com>
2018-02-12 23:32:22 +0100

    update admin page

--- public/admin.php
+++ public/admin.php
@@ -22,7 +22,7 @@
 
 <body>
 <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#"><?= APP_NAME; ?></a>
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#"><img src="<?= DIR_ASSETS; ?>/images/icon1.svg" width="24" alt=""> <?= APP_NAME; ?></a>
     <!--<input class="form-control form-control-dark w-100" type="text" placeholder="Rechercher" aria-label="Rechercher">-->
     <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
