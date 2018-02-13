07eec6f4995ad38ef14210fb32791ae2de682d2c
Yasmine AD <adrar.yasmine@gmail.com>
2018-02-13 18:14:31 +0100

    update css

---
+++ public/events.php
@@ -0,0 +1,59 @@
+<?php
+  require_once '../app/config/system.php';
+
+  require_once INCLUDES . '/default/header.php';
+?>
+<body>
+
+<section class="eventBox">
+  <?php //require_once INCLUDES . '/default/sidebar.php'; ?>
+  <div class="eventBox1">
+
+    <div class="box">
+      <img class="pictures" src="img/1.png" alt="">
+      <div class="lol">
+        <p class="vd">03/03/2018 </p>
+        <p class="za">Soirée d'hiver</p>
+      </div>
+
+    </div>
+    <div class="box">
+      <img class="pictures" src="img/3.png" alt="">
+      <div class="lol">
+        <p class="zd">03/03/2018</p>
+        <p class="za">Epithanie</p>
+      </div>
+    </div>
+
+
+  </div>
+
+  <div class="eventBox2">
+
+    <div class="box">
+      <img class="pictures" src="img/2.png" alt="">
+      <div class="lol">
+        <p class="md">31/03/2018</p>
+        <p class="za">Halloween</p>
+      </div>
+    </div>
+
+
+    <div class="box">
+      <img class="pictures" src="img/4.png" alt="">
+      <div class="lol">
+        <p class="ld">03/03/2018</p>
+        <p class="za">Soirée d'intégration</p>
+      </div>
+    </div>
+  </div>
+
+</section>
+
+</body>
+
+</html>
+
+<?php
+  require_once INCLUDES . '/default/footer.php';
+?>
