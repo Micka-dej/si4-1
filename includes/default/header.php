daf6b76fc59e7517950c8f232fb26e361518f77a
SundownDEV <anonhq789@protonmail.com>
2018-02-13 12:03:10 +0100

    update integration

--- includes/default/header.php
+++ includes/default/header.php
@@ -1,8 +1,30 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title><?= APP_NAME; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?= DIR_ASSETS; ?>/css/reset.css">
+    <link rel="stylesheet" href="<?= DIR_ASSETS; ?>/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet">
+    <title><?= (!empty($title)) ? APP_NAME . ' ' . $title : APP_NAME ?></title>
+    <link rel="icon" type="img/png" href="<?= DIR_ASSETS; ?>/img/favicon.png" />
 </head>
 <body>
+<header class="containerHeader">
+    <div class="logoNav">
+        <img class="<?= DIR_ASSETS; ?>/img/logo.svg" src="" alt="">
+    </div>
 
-<div class="header"></div>
+    <div>
+        <ul class="menu">
+            <li><a href='#' class='menuLI'>Web</a></li>
+            <li><a href='#' class='menuLI'>Grande Ecole</a></li>
+            <li><a href='#' class='menuLI'>Marketing</a></li>
+            <li><a href='#' class='menuLI'>Master</a></li>
+        </ul>
+    </div>
+    <div class="account">
+        <img src="<?= DIR_ASSETS; ?>/img/avatar.png" alt="">
+        <a href="#">Mon Compte</a>
+    </div>
+</header>
--- public/index.php
+++ public/index.php
@@ -6,8 +6,18 @@ require_once INCLUDES . '/default/header.php';
 
 ?>
 
-<div class="container">
-    <p>orgatic</p>
+<div class="inscriptionPage">
+    <div>
+        <h2 class='orgaticTitle'>Orga<span class='ticGreen'>'tic</span></h2>
+        <p class='mainParagraph'>Le réseaux des Heticiens pour les Heticiens</p>
+        <p class='mainParagraph'>Connectez vous pour suivre les actualités et les devoirs</p>
+    </div>
+    <div class="connexion">
+        <h2 class='connexionTitle'>Connexion</h2>
+        <input class='somethingTowrite' type="text">
+        <input class='somethingTowrite' type="text">
+        <a class='button' href="">VALIDER</a>
+    </div>
 </div>
 
 <?php
