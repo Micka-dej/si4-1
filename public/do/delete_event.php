d909fdd51d504eefb4474213f83bfb86901a6efe
Raphael <SundownDEV@users.noreply.github.com>
2018-02-15 23:29:14 +0100

    Update delete_event.php

--- public/do/delete_event.php
+++ public/do/delete_event.php
@@ -5,19 +5,33 @@ require_once '../../app/config/system.php';
 require_once '../../includes/admin/auth.php';
 
 if(!empty($_GET['id']) && !empty($_GET['csrf']) && $_GET['csrf'] == $_SESSION['csrf']) {
-    $req = $bdd->prepare('DELETE FROM `events` WHERE id = ?');
+    $req = $bdd->prepare('SELECT * FROM events WHERE id = :id');
     $req->execute([
-        $_GET['id']
+        ':id' => $_GET['id']
     ]);
+    $event = $req->fetch();
 
-    //supprimer la cover
+    if ($event) {
+        unlink(ROOT . "/assets/img/covers/" . $_GET['id'] . $event['cover']);
+    
+        $req = $bdd->prepare('DELETE FROM `events` WHERE id = :id');
+        $req->execute([
+            ':id' => $_GET['id']
+        ]);
 
-    $_SESSION['advert'] = [
-        'type' => 'success',
-        'message' => 'L\'évènement a bien été supprimée'
-    ];
+        $_SESSION['advert'] = [
+            'type' => 'success',
+            'message' => 'L\'évènement a bien été supprimée'
+        ];
 
-    header('Location: '. WEBROOT . '../manage_events.php');
+        header('Location: '. WEBROOT . '../manage_events.php');
+    }else{
+        $_SESSION['advert'] = [
+            'type' => 'error',
+            'message' => 'L\'évènement n\'existe pas'
+        ];
+        header('Location: '. WEBROOT . '../manage_events.php');
+    }
 }else{
     $_SESSION['advert'] = [
         'type' => 'error',
